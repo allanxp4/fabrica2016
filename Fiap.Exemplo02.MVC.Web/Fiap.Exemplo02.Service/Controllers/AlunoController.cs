0ca5cf8b0d9dc48c1a8a2babfd25249ca5d8fec8
Shift Professor <pfshift@fiap.com.br>
2016-11-29 18:22:09 -0200

    Ajax e Web.API

---
+++ Fiap.Exemplo02.MVC.Web/Fiap.Exemplo02.Service/Controllers/AlunoController.cs
@@ -0,0 +1,29 @@
+﻿using Fiap.Exemplo02.Dominio.Models;
+using Fiap.Exemplo02.MVC.Web.UnitsOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Fiap.Exemplo02.Service.Controllers
+{
+    public class AlunoController : ApiController
+    {
+        private UnitOfWork _unit = new UnitOfWork();
+
+        //GET api/aluno
+        public ICollection<Aluno> Get()
+        {
+            return _unit.AlunoRepository.Listar();
+        }
+
+        //GET api/aluno/{id}
+        public Aluno Get(int id)
+        {
+            return _unit.AlunoRepository.BuscarPorId(id);
+        }
+
+    }
+}
