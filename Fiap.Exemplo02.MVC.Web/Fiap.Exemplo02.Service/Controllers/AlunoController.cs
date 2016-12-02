efa87562c807ace272ad8e467da3f0e8563cb4fa
Shift Professor <pfshift@fiap.com.br>
2016-12-02 18:37:52 -0200

    API Client

--- Fiap.Exemplo02.MVC.Web/Fiap.Exemplo02.Service/Controllers/AlunoController.cs
+++ Fiap.Exemplo02.MVC.Web/Fiap.Exemplo02.Service/Controllers/AlunoController.cs
@@ -6,9 +6,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Fiap.Exemplo02.Service.Controllers
 {
+    [EnableCors(origins:"*",headers:"*",methods:"*")]
     public class AlunoController : ApiController
     {
         private UnitOfWork _unit = new UnitOfWork();
@@ -23,6 +25,42 @@ namespace Fiap.Exemplo02.Service.Controllers
         public Aluno Get(int id)
         {
             return _unit.AlunoRepository.BuscarPorId(id);
+        }
+
+        public IHttpActionResult Post(Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                _unit.AlunoRepository.Cadastrar(aluno);
+                _unit.Salvar();
+                var uri = Url.Link("DefaultApi", new { id = aluno.Id });
+                return Created<Aluno>(new Uri(uri), aluno);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        public IHttpActionResult Put(int id,Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                aluno.Id = id;
+                _unit.AlunoRepository.Alterar(aluno);
+                _unit.Salvar();
+                return Ok(aluno);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            _unit.AlunoRepository.Remover(id);
+            _unit.Salvar();
         }
 
     }
